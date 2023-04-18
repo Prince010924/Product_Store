13f1b6db8a6386a833a83bdb402e582059b1c670
prince010924 <neosenoamadi84@gmail.com>
2023-04-18 08:39:09 +0200

    edited the home components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ServicesfakestoreService } from 'src/app/servicesfakestore.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor(private fsService: ServicesfakestoreService){}
+
+  products: any[] =[];
+
+  ngOnInit(): void {
+      this.fsService.getProduct().subscribe((results: any) =>{
+        this.products = results
+        console.table(results)
+      })
+  }
 
 }
