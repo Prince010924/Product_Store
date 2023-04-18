3984cc1810f6d03ae020f130b5a5ae48142d61e2
prince010924 <neosenoamadi84@gmail.com>
2023-04-18 08:10:12 +0200

    added the getproduct function

--- src/app/servicesfakestore.service.ts
+++ src/app/servicesfakestore.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServicesfakestoreService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProduct(){
+    return this.http.get("https://fakestoreapi.com/products/");
+  }
 }
+
